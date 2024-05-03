ff0da8d037351db45a99b5e431942b37f75eb956
JorgeBerca <jorgeberecaparros@gmail.com>
2024-05-03 21:58:42 +0200

    Update ControllerMenu.java

--- src/controller/ControllerMenu.java
+++ src/controller/ControllerMenu.java
@@ -4,52 +4,56 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.scene.layout.BorderPane;
 
 public class ControllerMenu {
+
+    // Asumiendo que tienes un BorderPane en tu archivo FXML principal que actuará como contenedor principal
+    @FXML
+    private BorderPane mainContainer;
 
     @FXML
     public void captura() {
         loadScene("../vistas/pantalla captura.fxml");
     }
-    
+
+    @FXML
     public void equipo() {
         loadScene("../vistas/pantalla equipo.fxml");
     }
 
+    @FXML
     public void centropokemon() {
         loadScene("../vistas/pantalla centro pokemon.fxml");
     }
-    
+
+    @FXML
     public void combate() {
         loadScene("../vistas/Pantalla combate.fxml");
     }
-    
+
+    @FXML
     public void entrenamiento() {
         loadScene("../vistas/pantalla_entrenamiento.fxml");
     }
-    
+
+    @FXML
     public void crianza() {
         loadScene("../vistas/pantalla crianza.fxml");
     }
-    
+
+    @FXML
     public void mochila() {
         loadScene("../vistas/pantalla mochila tienda.fxml");
     }
     
-
     private void loadScene(String fxml) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
-            Parent root = loader.load();
-            Stage stage = new Stage(); // Crea un nuevo Stage o usa uno existente
-            stage.setScene(new Scene(root));
-            stage.show();
+            Parent newRoot = loader.load();
+            mainContainer.setCenter(newRoot); // Cambia el contenido del centro del BorderPane
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    // Añade aquí otros métodos de acción para los botones
 }
