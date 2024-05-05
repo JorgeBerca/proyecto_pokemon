7d7c930bd6d28a6ca6384ec0a1366bac298c6d09
Diegolive32 <Diegoantonio.s244148@cesurformacion.com>
2024-05-05 23:46:37 +0200

    cambios pequeños 2457
    he metido el boton para ir para atras en el pc y cambios en algunos fallos con botones

---
+++ src/controller/controllerpc.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class controllerpc {
+    @FXML
+    public void atras(javafx.event.ActionEvent event) {
+        try {
+            // Cargar la vista del menú
+            Parent menuView = FXMLLoader.load(getClass().getResource("/vistas/menu_final.fxml"));
+            
+            // Obtener el escenario actual y establecer la nueva escena
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(menuView));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error al cargar la pantalla del menú: " + e.getMessage());
+        }
+    }
+
+}
