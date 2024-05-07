dc672cca2598f852ddd7db80820723af03c10b4c
JorgeBerca <jorgeberecaparros@gmail.com>
2024-05-07 14:51:12 +0200

    Creado ControllerPc, pantallaPC, pantalla estadisticas y controller estadisticas

---
+++ src/controller/Controllerestadisticas.java
@@ -0,0 +1,21 @@
+package controller;
+
+import javafx.fxml.FXML;
+
+public class Controllerestadisticas {
+	
+	  @FXML
+      public void añadirEquipo() {
+		  
+      } 
+	  
+	  @FXML
+      public void vender() {
+		  
+      } 
+	  
+	  @FXML
+      public void liberar() {
+		  
+      }
+}
